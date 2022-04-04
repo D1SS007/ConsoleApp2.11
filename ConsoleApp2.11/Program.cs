75048ab671d86af932b66c64954bb3b2b219aad2
Alex Zverev <lordzver007@yandex.ru>
2022-04-04 12:28:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2.11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp2._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            //Цикл работает пока искомое число не найдено
+            bool isNumberFound = false;
+
+            //Число с которого идет отсчет
+            int startingNumber = 2;
+
+            //Степень в которую возводим число
+            int degree = 2;
+
+            //Подсчет количество степеней
+            int amountOfDegree = 0;
+
+            //Число, которое нужно превысить
+            int numberToGetOver = random.Next(0,1000);
+
+            while (!isNumberFound)
+            {
+                startingNumber *= degree;
+                amountOfDegree++;
+                if (startingNumber > numberToGetOver)
+                {
+                    amountOfDegree++;
+                    Console.WriteLine("Число которое загадали: " + numberToGetOver);
+                    Console.WriteLine("Количество степеней двойки: " + amountOfDegree);
+                    Console.WriteLine("Степень двойки , превосходящее число: " + startingNumber);
+                    break;
+                }
+            }
+        }
+    }
+}
